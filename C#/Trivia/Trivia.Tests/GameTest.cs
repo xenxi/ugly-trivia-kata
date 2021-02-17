3d5789b1ed6c4860f506aaed1aa2c9b18408a52e
Federico Ayala <fayala@voxelgroup.net>
2021-02-17 18:15:38 +0100

    Refactor: test game

--- C#/Trivia/Trivia.Tests/GameTest.cs
+++ C#/Trivia/Trivia.Tests/GameTest.cs
@@ -1,12 +1,28 @@
+using System;
+using System.IO;
+using System.Text;
+using ApprovalTests;
+using ApprovalTests.Namers;
+using ApprovalTests.Reporters;
 using Xunit;
 
 namespace Trivia.Tests
 {
+    [UseReporter(typeof(DiffReporter))]
     public class GameTest
     {
         [Fact]
-        public void Test1()
+        public void NewGame()
         {
+            var fakeOutput = new StringBuilder();
+            Console.SetOut(new StringWriter(fakeOutput));
+            Console.SetIn(new StringReader("a\n"));
+
+            new GameRunnerDelegate(new Random(8)).RunGame();
+
+            var output = fakeOutput.ToString();
+            Approvals.Verify(output);
+
         }
     }
 }
---
+++ C#/Trivia/Trivia/GameRunnerDelegate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Trivia
+{
+    public class GameRunnerDelegate
+    {
+        private static bool _notAWinner;
+        private Random rand;
+
+        public GameRunnerDelegate(Random random)
+        {
+            rand = random;
+        }
+
+        public void RunGame()
+        {
+            var aGame = new Game();
+
+            aGame.Add("Chet");
+            aGame.Add("Pat");
+            aGame.Add("Sue");
+
+            do
+            {
+                aGame.Roll(rand.Next(5) + 1);
+
+                if (rand.Next(9) == 7)
+                {
+                    _notAWinner = aGame.WrongAnswer();
+                }
+                else
+                {
+                    _notAWinner = aGame.WasCorrectlyAnswered();
+                }
+            } while (_notAWinner);
+        }
+    }
+}
