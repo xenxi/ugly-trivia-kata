c9235c29e2202c212b127eb0f01d740818aec091
andiaz <andiaz@aidacanarias.com>
2021-10-01 06:48:23 +0200

    extract method MoveNewPlace

--- C#/Trivia/Trivia/Game.cs
+++ C#/Trivia/Trivia/Game.cs
@@ -65,13 +65,8 @@ namespace Trivia {
                     _isGettingOutOfPenaltyBox = true;
 
                     Console.WriteLine(_players[_currentPlayer] + " is getting out of the penalty box");
-                    _places[_currentPlayer] = _places[_currentPlayer] + roll;
-                    if (_places[_currentPlayer] > 11) _places[_currentPlayer] = _places[_currentPlayer] - 12;
 
-                    Console.WriteLine(_players[_currentPlayer]
-                                      + "'s new location is "
-                                      + _places[_currentPlayer]);
-                    Console.WriteLine("The category is " + CurrentCategory());
+                    MoveNewPlace(roll);
                     AskQuestion();
                 }
             }
@@ -85,6 +80,16 @@ namespace Trivia {
                 Console.WriteLine("The category is " + CurrentCategory());
                 AskQuestion();
             }
+        }
+
+        private void MoveNewPlace(int roll) {
+            _places[_currentPlayer] = _places[_currentPlayer] + roll;
+            if (_places[_currentPlayer] > 11) _places[_currentPlayer] = _places[_currentPlayer] - 12;
+
+            Console.WriteLine(_players[_currentPlayer]
+                              + "'s new location is "
+                              + _places[_currentPlayer]);
+            Console.WriteLine("The category is " + CurrentCategory());
         }
 
         private static bool CanGoOutFromPenaltyBox(int roll) {
